2403e4ff3aa8bc688bde104cd990989597ee23db
Artem Fedotov <hahahappyface@gmail.com>
2019-03-23 11:38:37 +0300

    feat: add function code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    var obj = {
+         H: 50,
+         Q: 25,
+         D: 10,
+         N: 5,
+         P: 1    
+      }
+     
+    if (currency > 10000) {
+      var errobj = {
+          error: "You are rich, my friend! We don\'t have so much coins for exchange"
+      }
+      return (errobj);
+    }
+      let resultobj = {};
+      for (var prop in obj) {
+        if (currency <= 0) break;
+        let c = obj[prop];
+        obj[prop] = Math.floor(currency/c);
+        currency = currency-c*obj[prop];
+        if (obj[prop] != 0) {
+          resultobj[prop] = obj[prop];
+         }
+      }
+      return (resultobj); 
+    }
